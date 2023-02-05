3323f6735a7ae97a75ddf9a75a2f2ee6ae9cf9a4
Sajad <Sajad@DESKTOP-HEEH2BS>
2023-02-05 15:53:09 +0330

    Add project files.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+MyClass obj = new MyClass("sajad", "mohamadi");
+obj.Dispaly();
+public class MyClass
+{
+    private string name;
+    private string description;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+        set
+        {
+            description = value;
+        }
+    }
+    public MyClass(string name,string description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+    public void Dispaly()
+    {
+        Console.WriteLine("name is:{0} and description is:{1}", name, description);
+
+    }
+}
